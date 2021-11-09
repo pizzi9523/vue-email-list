5e0ba555a665de057e7285d8cfb4b3f1671a7bf2
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-09 15:15:52 +0100

    BONUS show email completely generated & error

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const app = new Vue({
     el: "#root",
     data: {
         emails: [],
+        error: "",
 
     },
 
@@ -9,8 +10,15 @@ const app = new Vue({
         for (let i = 0; i < 10; i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(myResp => {
                 //console.log(myResp);
+                //console.log(myResp.data.response);
                 this.emails.push(myResp.data.response)
+                // if (myResp.request.readyState === 4 && myResp.status === 200) {
+                //     
+                // }
                 //console.log(this.emails);
+            }).catch(e => {
+                console.error(e, "OPS!");
+                this.error = e
             })
 
         }
