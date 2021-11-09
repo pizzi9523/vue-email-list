9d9f733feac12512ad20648e430b326335714b11
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-09 15:02:48 +0100

    create 10 emails from api and add to html

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const app = new Vue({
     el: "#root",
     data: {
+        emails: [],
 
     },
 
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(myResp => {
+                //console.log(myResp);
+                this.emails.push(myResp.data.response)
+                //console.log(this.emails);
+            })
+
+        }
+
+    }
 })
